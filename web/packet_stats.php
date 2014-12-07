2af910637511166d59146420a472b793515f4e05
Bartek Bok <bartek.bok@gmail.com>
2014-12-07 19:19:24 +0100

    Customize packet stats output

--- web/packet_stats.php
+++ web/packet_stats.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$result_type= $_GET['t'] ?: "txt";
+
 if (!isset($_GET['p'])) {
     header("HTTP/1.1 500 You get nothing!");
 } else {
@@ -13,11 +15,24 @@ if (!isset($_GET['p'])) {
     if ($type != "hash") {
         header("HTTP/1.1 500 LOL NOPE");
     } else {
-        header('Content-Type: text/plain');
         $values = $redis->hgetall($key);
         ksort($values);
-        foreach ($values as $k => $v) {
-            print "$k\t$v\n";
+
+        if ($result_type == "txt") {
+            header('Content-Type: text/plain');
+            foreach ($values as $k => $v) {
+                print "$k\t$v\n";
+            }
+        } elseif ($result_type == "csv") {
+            header('Content-Type: text/csv');
+            foreach ($values as $k => $v) {
+                print "\"$k\",$v\r\n";
+            }
+        } elseif ($result_type == "json") {
+            header('Content-Type: application/json');
+            print json_encode($values, JSON_PRETTY_PRINT);
+        } else {
+            header("HTTP/1.1 500 We don't do THAT!!!");
         }
 
     }
